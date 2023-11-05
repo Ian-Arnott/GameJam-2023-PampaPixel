c5449b9f18f86c3aaaee20d033679fc564d44391
AdolfoRosauer1 <jurosauer@itba.edu.ar>
2023-11-05 10:16:11 -0300

    death zones

---
+++ Assets/Scripts/Triggers/DeathTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+    public class DeathTrigger : MonoBehaviour
+    {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            EventManager.instance.EventGameOver(false);
+            Destroy(other.gameObject);
+        }
+
+    }
+}
